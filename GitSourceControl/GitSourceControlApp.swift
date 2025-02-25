4b18120393a5b5f33cdc3a0a6fc6e5609d9f5bf5
imoofs <imoofs@gmail.com>
2025-02-25 14:29:54 +0900

    Initial Commit

---
+++ GitSourceControl/GitSourceControlApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitSourceControlApp.swift
+//  GitSourceControl
+//
+//  Created by Hungu Lim on 2/25/25.
+//
+
+import SwiftUI
+
+@main
+struct GitSourceControlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
