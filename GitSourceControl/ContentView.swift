84227a6d82636fc4b49af53f0756326b71be0890
imoofs <imoofs@gmail.com>
2025-02-26 09:37:09 +0900

    Another commit

--- GitSourceControl/ContentView.swift
+++ GitSourceControl/ContentView.swift
@@ -14,10 +14,6 @@ struct ContentView: View {
                 .font(.largeTitle)
                 .foregroundStyle(.tint)
             Text("Happy Valentine's day!")
-            
-            Button("Click me!") {
-                
-            }
         }
         .padding()
     }
