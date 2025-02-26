36bf395afbda0f240dc6e4be34fae991d562d60f
imoofs <imoofs@gmail.com>
2025-02-26 09:48:37 +0900

    Add HomeView

---
+++ GitSourceControl/HomeView.swift
@@ -0,0 +1,18 @@
+//
+//  HomeView.swift
+//  GitSourceControl
+//
+//  Created by Hungu Lim on 2/26/25.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text("Screen 2")
+    }
+}
+
+#Preview {
+    HomeView()
+}
