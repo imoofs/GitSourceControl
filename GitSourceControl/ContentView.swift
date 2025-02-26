35a6813e32b71e5fc3b57d8b973a4785c2351616
imoofs <imoofs@gmail.com>
2025-02-26 09:44:03 +0900

    Add click me button!

--- GitSourceControl/ContentView.swift
+++ GitSourceControl/ContentView.swift
@@ -14,6 +14,10 @@ struct ContentView: View {
                 .font(.largeTitle)
                 .foregroundStyle(.tint)
             Text("Happy Valentine's day!")
+            
+            Button("Click Me") {
+                
+            }
         }
         .padding()
     }
