450d339957e9365b619f96aade7947ffede383c5
imoofs <imoofs@gmail.com>
2025-02-27 09:06:43 +0900

    [Clean] Updates to documention

--- GitSourceControl/ContentView.swift
+++ GitSourceControl/ContentView.swift
@@ -5,6 +5,15 @@
 //  Created by Hungu Lim on 2/25/25.
 //
 
+/*
+ Clone = Copying the repo locally
+ Commit = Save ("checkpoint") on our current branch
+ Stage = Prepare changes for later
+ Push = Send local commits to remote repo
+ Pull = Fetch remote commits to local repo
+
+ */
+
 import SwiftUI
 
 struct ContentView: View {
