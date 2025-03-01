8528d32a61b6aa9ba1b09a45aae7275d4527a9d2
imoofs <imoofs@gmail.com>
2025-03-01 10:00:55 +0900

    [Feature] Fix title

--- GitSourceControl/HomeView.swift
+++ GitSourceControl/HomeView.swift
@@ -8,7 +8,7 @@
 import SwiftUI
 
 struct HomeView: View {
-    @State private var title: String = "Hello, there !"
+    @State private var title: String = "Hello, moof!"
     var body: some View {
         VStack(spacing: 8) {
             Text("Hi")
