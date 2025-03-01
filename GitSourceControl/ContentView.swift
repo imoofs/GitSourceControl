453285ac7eb6d2a345db01438ce47f9a99a48f76
imoofs <imoofs@gmail.com>
2025-03-01 09:47:34 +0900

    [Clean] Update dockumentation

--- GitSourceControl/ContentView.swift
+++ GitSourceControl/ContentView.swift
@@ -7,11 +7,13 @@
 
 /*
  Clone = Copying the repo locally
- Commit = Save ("checkpoint") on our current branch
+ Commit = Save ("checkpoint") changes on our current branch
  Stage = Prepare changes for later
  Push = Send local commits to remote repo
  Pull = Fetch remote commits to local repo
-
+ Merge = Joining two different branch
+ Rebase = Moving one branch on top of another branch
+ Cherry picking = Duplicating (copying) one commit from one branch to another
  */
 
 import SwiftUI
