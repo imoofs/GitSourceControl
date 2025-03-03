4e55b79c6ea6ea1c661b616e2f868ba0083c4817
imoofs <imoofs@gmail.com>
2025-03-03 12:47:29 +0900

    [Clean] Update value name to "isPremium"

--- GitSourceControl/SettingsView.swift
+++ GitSourceControl/SettingsView.swift
@@ -8,7 +8,7 @@
 import SwiftUI
 
 struct SettingsView: View {
-    @State private var doSomething: Bool = false
+    @State private var isPremium: Bool = false
     
     var body: some View {
         Text("Settings!")
