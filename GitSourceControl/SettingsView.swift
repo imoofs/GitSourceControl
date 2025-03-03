12a984b7472aaf68953517ba7c8474998dfa14c1
imoofs <imoofs@gmail.com>
2025-03-03 12:34:06 +0900

    [Feature] Add settings feature

--- GitSourceControl/SettingsView.swift
+++ GitSourceControl/SettingsView.swift
@@ -8,8 +8,10 @@
 import SwiftUI
 
 struct SettingsView: View {
+    @State private var doSomething: Bool = false
+    
     var body: some View {
-        Text("Settings View ")
+        Text("Settings!")
     }
 }
 
